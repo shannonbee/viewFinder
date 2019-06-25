38b18b0dab746c5f0e3d8b63ea8bf0c28a5009d8
shannonbee <shannonbenyamin@gmail.com>
2019-06-25 10:10:28 -0400

    ViewFinder

---
+++ Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+//
+//var str = "Hello, playground"
+//
+//var inputOne = [4,6,8,10]
+//
+//func Product (inputOne)
+
+
+func findSmallest(input:[Int]) -> Int
+var smallest = input [0]
+for nums in input {
+    if nums < smallest {
+        smallest = nums
+    }
+    
+
+return smallest
+
+}
+
+func findBiggest(input: [Int])
+
